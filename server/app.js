5cf8929172fa9905d5fc90566a97b44e407ffd66
Obaida <91075544+Abdullah-Wael-Ahmed@users.noreply.github.com>
2025-09-10 13:53:05 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,10 @@ const express = require('express')
 const server = express()
 
 
+server.get("/hello", (req, res) => {
+    res.send("Hello Kareem hossam")
+})
+
 server.listen(5000, () => {
     console.log("server running on port 5000");
 })
