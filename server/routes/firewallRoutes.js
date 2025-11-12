db97cf852b29b999bd2ab945e646c12defe5f1c3
Obaida <91075544+Abdullah-Wael-Ahmed@users.noreply.github.com>
2025-11-13 00:01:15 +0200

    Update firewallRoutes.js

--- server/routes/firewallRoutes.js
+++ server/routes/firewallRoutes.js
@@ -1,14 +1,26 @@
 const express = require("express");
-const {exec} =  require('child_process')
-const { stderr, stdout } =  require('process');
+const { exec } = require('child_process')
+const { stderr, stdout } = require('process');
 
 const router = express.Router();
 
-router.get("/list", (req, res) => {
-    exec('sudo iptables -L -n --line-numbers', (error, stdout, stderr) => {
-        if (error) return res.status(500).json({error: stderr});
-        res.json({rules: stdout})
-    })
-})
+
+const runAsNodeUser = (command, res, successMessage) => {
+    const fullCmd = `sudo - u nodeuser ${command}`;
+    exec(fullCmd, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${stderr}`);
+            return res.status(500).json({ error: stderr });
+        }
+        res.json({ message: successMessage, output: stdout });
+    });
+};
+
+
+router.get('/list', (req, res) => {
+    const cmd = "/usr/sbin/iptables -L -n --line-numbers";
+    runAsNodeUser(cmd, res, 'Rules listed successfully');
+});
+
 
 module.exports = router;
