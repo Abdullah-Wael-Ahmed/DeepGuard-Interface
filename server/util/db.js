352fda49471bc21f5fe08e10a9044ccf4bd4308c
Obaida <91075544+Abdullah-Wael-Ahmed@users.noreply.github.com>
2026-03-05 02:51:00 +0200

    migrating to mysql

--- server/util/db.js
+++ server/util/db.js
@@ -1,9 +1,14 @@
 const { Sequelize } = require("sequelize");
 
+console.log(process.env.MYSQL_DATABASE,process.env.MYSQL_USER,process.env.MYSQL_PASSWORD)
+
 const db = new Sequelize({
-    dialect: "sqlite",
-    storage: "./deepguard.sqlite",
-    logging: false
+    host: 'db',
+    dialect: "mysql",
+    logging: false,
+    database: process.env.MYSQL_DATABASE,
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD
 });
 
 module.exports = db ;
